fd444d7ccc4bc82d74596b079b60b11792ac124f
Alan Bernal <abernalespinoza@outlook.com>
2023-10-11 03:14:05 -0500

    Implementación de sistema Register guiado por test.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout']);
+Route::post('register', [RegisterController::class, 'register']);
