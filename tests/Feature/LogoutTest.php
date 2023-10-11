433c1fb6e655d315c0809c5d5a9d90854efa4bcd
Alan Bernal <abernalespinoza@outlook.com>
2023-10-11 03:05:43 -0500

    Test para desarrollo de sistema de logout.

---
+++ tests/Feature/LogoutTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('logout method revokes the current user token', function () {
+    // Ejemplo de datos de prueba
+    $userData = [
+        'email' => 'test@example.com',
+        'password' => 'secret-password',
+    ];
+
+    // Crear un usuario de prueba
+    $user = User::factory()->create($userData);
+
+    // Autenticar al usuario y obtener un token de acceso
+    $token = $user->createToken('test-device')->plainTextToken;
+
+    // Simular una solicitud HTTP POST al método 'logout' con el token de acceso
+    $response = $this
+        ->withHeaders(['Accept' => 'application/json'])
+        ->post('/api/logout', [], ['Authorization' => "Bearer $token"]);
+
+    // Verificar que la respuesta sea exitosa (código 200)
+    $response->assertStatus(200);
+
+    // Verificar que el token se ha revocado
+    $this->assertEmpty($user->tokens);
+});
