601a85ca040f64f3bdbc4c38c3b3d8be1cc60568
Alan Bernal <abernalespinoza@outlook.com>
2023-10-11 03:52:12 -0500

    Test para desarrollo de listado de registro por id de modelo Album.

--- tests/Feature/Models/AlbumTest.php
+++ tests/Feature/Models/AlbumTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-it('Puede obtener una lista de álbumes', function () {
+it('authenticated user can fetch a list of albums', function () {
     // Crear algunos álbumes de prueba en la base de datos
     Artist::factory()->count(5)->create();
     Album::factory()->count(5)->create();
@@ -25,4 +25,35 @@ it('Puede obtener una lista de álbumes', function () {
 
     // Verificar que el cuerpo de la respuesta contiene datos de álbumes
     $response->assertJsonCount(5, 'data');
+});
+
+it(' authenticated user can fetch details of a single album', function () {
+    // Crear un álbum de prueba en la base de datos
+    Artist::factory()->create();
+    $album = Album::factory()->create();
+
+    // Crear un usuario y autenticarlo
+    $user = User::factory()->create();
+    $token = $user->createToken('test-token')->plainTextToken;
+
+    // Hacer una solicitud GET a la ruta de la API con el token de autenticación para obtener detalles del álbum
+    $response = $this->getJson("api/albums/{$album->id}", ['Authorization' => 'Bearer ' . $token]);
+
+    // Verificar que la solicitud fue exitosa (código de respuesta 200)
+    $response->assertStatus(200);
+
+    // Verificar que el cuerpo de la respuesta contiene datos del álbum
+    $response->assertJson(['data' => $album->toArray()]);
+});
+
+it('returns a 404 response when trying to fetch details of a non-existing album', function () {
+    // Crear un usuario y autenticarlo
+    $user = User::factory()->create();
+    $token = $user->createToken('test-token')->plainTextToken;
+
+    // Intentar una solicitud GET a la ruta de la API con el token de autenticación para obtener detalles de un album que no existe en la base de datos
+    $response = $this->getJson('/api/albums/999', ['Authorization' => 'Bearer ' . $token]);
+
+    // Verificar que la solicitud devuelva un código de respuesta 404
+    $response->assertStatus(404);
 });
